729de4fb0f7f7579165f176fe556088950ac337b
Odohi David <odohi.david@gmail.com>
2020-02-05 16:05:34 +0100

    create task model

---
+++ src/main/java/com/david/todosq4/models/Task.java
@@ -0,0 +1,81 @@
+package com.david.todosq4.models;
+
+import java.sql.Time;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.validator.constraints.Length;
+
+/**
+ * Task
+ */
+@Entity
+@Table(name = "tasks")
+public class Task {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+
+  @NotBlank
+  @NotNull
+  @Length(max = 150)
+  private String title;
+  private String description;
+  // @JsonAlias({ "stats", "statuses" })
+  private String status;
+
+  @CreationTimestamp
+  @Column(name = "created_at", nullable = false)
+  private Time createdAt;
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public String getStatus() {
+    return status;
+  }
+
+  public void setStatus(String status) {
+    this.status = status;
+  }
+
+  public Time getCreatedAt() {
+    return createdAt;
+  }
+
+  public void setCreatedAt(Time createdAt) {
+    this.createdAt = createdAt;
+  }
+
+}
