ed5f902b62b1768e0850701386822db1abec568c
Odohi David <odohi.david@gmail.com>
2020-02-06 12:21:26 +0100

    create a custom repository methodf

--- src/main/java/com/david/todosq4/repository/TaskRepository.java
+++ src/main/java/com/david/todosq4/repository/TaskRepository.java
@@ -1,5 +1,7 @@
 package com.david.todosq4.repository;
 
+import java.util.List;
+
 import com.david.todosq4.models.Task;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,5 +10,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * TaskRepository
  */
 public interface TaskRepository extends JpaRepository<Task, Integer> {
-  // public Task findByStatus();
+  public List<Task> findByStatus(String status);
+
 }
